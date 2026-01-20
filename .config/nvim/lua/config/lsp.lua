774951a1fce42562eac0bbcd06f7a7f1709b3062
dernasherbrezon <dernasherbrezon@gmail.com>
2026-01-20 18:43:58 +0000

    initial

---
+++ .config/nvim/lua/config/lsp.lua
@@ -0,0 +1,142 @@
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+vim.lsp.config('*', {
+  capabilities = capabilities,
+  root_markers = { '.git' },
+})
+
+vim.lsp.config['lua_ls'] = {
+  -- Command and arguments to start the server.
+  cmd = { 'lua-language-server' },
+  -- Filetypes to automatically attach to.
+  filetypes = { 'lua' },
+  -- Sets the "root directory" to the parent directory of the file in the
+  -- current buffer that contains either a ".luarc.json" or a
+  -- ".luarc.jsonc" file. Files that share a root directory will reuse
+  -- the connection to the same LSP server.
+  -- Nested lists indicate equal priority, see |vim.lsp.Config|.
+  root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
+  -- Specific settings to send to the server. The schema for this is
+  -- defined by the server. For example the schema for lua-language-server
+  -- can be found here https://raw.githubusercontent.com/LuaLS/vscode-lua/master/setting/schema.json
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+      }
+    }
+  },
+  capabilities = capabilities,
+}
+vim.lsp.enable('lua_ls')
+
+vim.lsp.config['cmake'] = {
+  cmd = { 'cmake-language-server' },
+  filetypes = { 'cmake' },
+  root_markers = { 'CMakePresets.json', 'CTestConfig.cmake', '.git', 'build', 'cmake' },
+  init_options = {
+    buildDirectory = 'build',
+  },
+}
+vim.lsp.enable('cmake')
+
+local function switch_source_header(bufnr)
+  local method_name = 'textDocument/switchSourceHeader'
+  local client = vim.lsp.get_clients({ bufnr = bufnr, name = 'clangd' })[1]
+  if not client then
+    return vim.notify(('method %s is not supported by any servers active on the current buffer'):format(method_name))
+  end
+  local params = vim.lsp.util.make_text_document_params(bufnr)
+  client.request(method_name, params, function(err, result)
+    if err then
+      error(tostring(err))
+    end
+    if not result then
+      vim.notify('corresponding file cannot be determined')
+      return
+    end
+    vim.cmd.edit(vim.uri_to_fname(result))
+  end, bufnr)
+end
+
+local function symbol_info()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local clangd_client = vim.lsp.get_clients({ bufnr = bufnr, name = 'clangd' })[1]
+  if not clangd_client or not clangd_client.supports_method 'textDocument/symbolInfo' then
+    return vim.notify('Clangd client not found', vim.log.levels.ERROR)
+  end
+  local win = vim.api.nvim_get_current_win()
+  local params = vim.lsp.util.make_position_params(win, clangd_client.offset_encoding)
+  clangd_client.request('textDocument/symbolInfo', params, function(err, res)
+    if err or #res == 0 then
+      -- Clangd always returns an error, there is not reason to parse it
+      return
+    end
+    local container = string.format('container: %s', res[1].containerName) ---@type string
+    local name = string.format('name: %s', res[1].name) ---@type string
+    vim.lsp.util.open_floating_preview({ name, container }, '', {
+      height = 2,
+      width = math.max(string.len(name), string.len(container)),
+      focusable = false,
+      focus = false,
+      border = 'single',
+      title = 'Symbol Info',
+    })
+  end, bufnr)
+end
+
+---@class ClangdInitializeResult: lsp.InitializeResult
+---@field offsetEncoding? string
+vim.lsp.config['clangd'] = {
+  cmd = { 'clangd' },
+  filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda' },
+  root_markers = {
+    '.clangd',
+    '.clang-tidy',
+    '.clang-format',
+    'compile_commands.json',
+    'compile_flags.txt',
+    'configure.ac', -- AutoTools
+    '.git',
+  },
+  capabilities = {
+    textDocument = {
+      completion = {
+        editsNearCursor = true,
+      },
+    },
+    offsetEncoding = { 'utf-8', 'utf-16' },
+  },
+  ---@param client vim.lsp.Client
+  ---@param init_result ClangdInitializeResult
+  on_init = function(client, init_result)
+    if init_result.offsetEncoding then
+      client.offset_encoding = init_result.offsetEncoding
+    end
+  end,
+  on_attach = function(_, bufnr)
+    vim.api.nvim_buf_create_user_command(bufnr, 'LspClangdSwitchSourceHeader', function()
+      switch_source_header(bufnr)
+    end, { desc = 'Switch between source/header' })
+
+    vim.api.nvim_buf_create_user_command(bufnr, 'LspClangdShowSymbolInfo', function()
+      symbol_info()
+    end, { desc = 'Show symbol info' })
+  end,
+}
+vim.lsp.enable('clangd')
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function()
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, {desc = "Hover"})
+    vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {desc="Go to Definition"})
+    vim.keymap.set('n', '<leader>gi', vim.lsp.buf.implementation, {desc="Go to Implementation"})
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {desc="Code action"})
+    vim.keymap.set('n', '<leader>cr', vim.lsp.buf.rename, {desc="Rename"})
+  end
+})
+
+vim.diagnostic.config({
+  virtual_text = true,
+  update_in_insert = true
+})
