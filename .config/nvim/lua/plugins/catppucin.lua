b47d14c85d9fb2a0c0883fefe27186dde14405eb
dernasherbrezon <dernasherbrezon@gmail.com>
2026-02-02 20:29:56 +0000

    configure java colours

--- .config/nvim/lua/plugins/catppucin.lua
+++ .config/nvim/lua/plugins/catppucin.lua
@@ -5,6 +5,35 @@ return {
   priority = 1000,
   tag = "v1.11.0",
   config = function()
+    require("catppuccin").setup({
+      flavour = "mocha",
+      no_italic = false,
+      no_bold = false,
+      styles = {
+        keywords = { "bold" },
+      },
+      highlight_overrides = {
+        all = function(colors)
+          return {
+            Type = { fg = colors.text },
+            Function = { fg = colors.text },
+            ["@constant.java"] = { fg = colors.blue, bold = true },
+            ["@lsp.typemod.property.static.java"] = { fg = colors.blue, bold = true },
+            ["@property"] = { fg = colors.text },
+            ["@parameter"] = { fg = colors.text },
+            ["@type.builtin.java"]     = { fg = colors.mauve, bold = true },
+            ["@keyword.operator.java"] = { fg = colors.mauve, bold = true },
+            ["@variable.member.java"] = { fg = colors.text },
+            ["@variable.parameter.java"] = { fg = colors.text },
+            ["@function.method.java"] = { fg = colors.text },
+            ["@function.method.call.java"] = { fg = colors.text },
+            ["@lsp.type.namespace.java"] = { fg = colors.text },
+--            ["@number.java"] = { fg = colors.text },
+          }
+        end
+      },
+      compile_path = vim.fn.stdpath "cache" .. "/catppuccin"
+    })
     vim.cmd.colorscheme "catppuccin"
   end
 }
---
+++ .config/nvim/test.java
@@ -0,0 +1,17 @@
+
+public class MyClass {
+
+  private final static int FUN = 12;
+  private String someVariable;
+
+  public static void generate(int nothing) throws Exception {
+    String res = new String("variable");
+    System.out.println(res.length());
+    MyClass test = new MyClass();
+    test.calculate();
+  }
+
+  public Integer calculate() {
+    return 0;
+  }
+}
